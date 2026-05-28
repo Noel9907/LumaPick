88c2005ef0b141d5cc08f306d771340605461dfd
Noel9907 <noelskocheekkaran@gmail.com>
2026-05-28 16:37:00 +0530

    like image api

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from services.album import album
-from services.album import GetImage
+from services.album import album,likeImage,GetImage
 router = APIRouter()
 
-router.add_api_route("/album",album,methods=['GET'])
+router.add_api_route("/album",album,methods=['POST'])
 router.add_api_route("/image",GetImage,methods=['GET'])
+router.add_api_route("/like",likeImage,methods=['POST'])
--- app/services/album.py
+++ app/services/album.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 
 load_dotenv()
-conn = sqlite3.connect("test.db")
+conn = sqlite3.connect("test.db", check_same_thread=False)
 cursor = conn.cursor()
 
 api = os.getenv("GOOGLE_DRIVE_KEY")
@@ -17,9 +17,12 @@ class Albumlink(BaseModel):
 class Gallery(BaseModel):
     name: str
 
+class Like(BaseModel):
+    id: str
+
 
 async def album(data : Albumlink):
-    cursor.execute("""CREATE TABLE IF NOT EXISTS studio (id INTEGER PRIMARY KEY,file_id TEXT,name,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS studio (id INTEGER PRIMARY KEY,file_id TEXT UNIQUE,name TEXT,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)""")
     link = data.folderlink
     m=link.find("folders/")
     l=link.find("?")
@@ -28,7 +31,7 @@ async def album(data : Albumlink):
     response = requests.get(f"https://www.googleapis.com/drive/v3/files?q='{link[m+8:l]}'+in+parents+and+mimeType+contains+'image/'&fields=files(id,name,mimeType)&key={api}").json()
     cursor.execute("""select * from studio""")
     for i in response["files"]:
-        cursor.execute("""Insert into studio(file_id,name) VALUES (?,?)""",(i["id"],name))
+        cursor.execute("""Insert OR IGNORE into studio(file_id,name) VALUES (?,?)""",(i["id"],name))
     conn.commit()
     cursor.execute("""select * from studio""")
     return(cursor.fetchall())
@@ -41,5 +44,13 @@ async def GetImage(data:Gallery):
         dic.append(f"https://drive.google.com/thumbnail?id={img[1]}&sz=w1000")
     return(dic)
 
-    
 
+def likeImage(data: Like):
+    cursor.execute("""CREATE TABLE IF NOT EXISTS like (id INTEGER PRIMARY KEY,file_id TEXT UNIQUE,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)""")
+    cursor.execute("""INSERT OR IGNORE INTO  like(file_id) values (?)""",(data.id,))
+    conn.commit()
+    cursor.execute("""select * from like""")
+    return(cursor.fetchall())
+
+    
+#download imaage : f"https://drive.google.com/uc?export=download&id={file_id}"
